b183a83b5b8405150f12c89a495461cba4982aed
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2022-07-02 16:35:16 +0800

    initial

---
+++ BlankApp1/BlankApp1/ViewModels/MainPageViewModels.cs
@@ -0,0 +1,43 @@
+﻿namespace BlankApp1.ViewModels
+{
+    using System.ComponentModel;
+    using Prism.Commands;
+    using Prism.Events;
+    using Prism.Navigation;
+    using Prism.Services;
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService navigationService;
+        public string MyEntry { get; set; }
+        public string MyLabel { get; set; } = "test";
+        public DelegateCommand BtnCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            BtnCommand = new DelegateCommand(OnButtonClick);
+        }
+
+        public void OnButtonClick()
+        {
+            MyLabel = MyEntry;
+            OnPropertyChanged("MyLabel");
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
