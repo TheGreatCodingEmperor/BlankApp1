22b625e84eb4cc8f29c4039fb0f09f71d4538112
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2022-07-08 21:09:12 +0800

    1. 算術遊戲完成

--- BlankApp1/BlankApp1/ViewModels/MainPageViewModels.cs
+++ BlankApp1/BlankApp1/ViewModels/MainPageViewModels.cs
@@ -1,29 +1,54 @@
 ﻿namespace BlankApp1.ViewModels
 {
+    using System;
     using System.ComponentModel;
     using Prism.Commands;
     using Prism.Events;
     using Prism.Navigation;
     using Prism.Services;
+    using PropertyChanged;
+
     public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
         private readonly INavigationService navigationService;
-        public string MyEntry { get; set; }
-        public string MyLabel { get; set; } = "test";
-        public DelegateCommand BtnCommand { get; set; }
+        public string MyQuestion { get; set; }
+        public string MyAnswer { get; set; }
+        public string MyResult { get; set; }
+        public DelegateCommand ReGen { get; set; }
+        public DelegateCommand SendAnswer { get; set; }
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public Random rand = new Random();
 
         public MainPageViewModel(INavigationService navigationService)
         {
+            QuestionGen();
             this.navigationService = navigationService;
-            BtnCommand = new DelegateCommand(OnButtonClick);
+            ReGen = new DelegateCommand(QuestionGen);
+            SendAnswer = new DelegateCommand(OnButtonClick);
+        }
+
+        public void QuestionGen()
+        {
+            Value1 = rand.Next(0,100);
+            Value2 = rand.Next(0, 100);
+            MyQuestion = $"{Value1} + {Value2} = ?";
+            MyAnswer = "";
         }
 
         public void OnButtonClick()
         {
-            MyLabel = MyEntry;
-            OnPropertyChanged("MyLabel");
+            if(MyAnswer.Trim() == (Value1+ Value2).ToString())
+            {
+                MyResult = "恭喜答對!!";
+                QuestionGen();
+            }
+            else
+            {
+                MyResult = "回答錯誤!!";
+            }
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters)
@@ -34,10 +59,10 @@
         {
         }
 
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        //protected virtual void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
 
     }
 }
